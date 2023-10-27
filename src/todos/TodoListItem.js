53ca3030f99693b58072c8cfce96aaa9a3cc54fd
Sandeep Prajapati <sandeep.p4856@gmail.com>
2023-10-28 01:15:15 +0530

    added todo appwith react redux and reduc-persist functionality

---
+++ src/todos/TodoListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './TodoListItem.css';
+
+const TodoListItem = ({ todo, onRemovePressed, onCompletedPressed }) => (
+    <div className="todo-item-container">
+        <h4>{todo.text}</h4>
+        <div className="buttons-container">
+            {todo.isCompleted
+                ? null 
+                :
+                <button 
+                className="completed-button"
+                onClick={() => onCompletedPressed(todo.text)}
+                >
+                    Mark as Completed
+                </button>
+            }
+            <button 
+                className="remove-button"
+                onClick={() => onRemovePressed(todo.text)}
+            >
+                Remove
+            </button>
+        </div>
+    </div>
+);
+
+export default TodoListItem
